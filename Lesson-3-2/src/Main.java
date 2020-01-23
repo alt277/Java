848e140366d038d739aae84be9e10efd7e629288
alt277 <altima875@gmail.com>
2020-01-23 20:49:55 +0300

    comment

--- Lesson-3-2/src/Main.java
+++ Lesson-3-2/src/Main.java
@@ -17,6 +17,15 @@ public class Main {
         list.addPhone("8 903 187-54-27","Senya");
         list.findPhone("Senya");
         list.findPhone("Tanya");
+
+        PhoneList2 list2 = new PhoneList2();
+        list2.addPhone("Anya","8 903 187-34-25");
+        list2.addPhone("Anya","8 103 187-04-27");
+        list2.addPhone("Anya","8 978 187-24-21");
+        list2.addPhone("Tanya","8 903 007-27-91");
+        list2.addPhone("Tanya","8 978 187-277-11");
+        list2.findPhone("Anya");
+
         }
 
     }
